3d6688a6b80d61145477afff6bfb2da54432ebd0
neverlands90 <594970080@qq.com>
2020-06-06 18:57:15 +0800

    练习1.25 sales_item

---
+++ SalesItem/main.cpp
@@ -0,0 +1,39 @@
+﻿// CppPrimer.cpp: 定义应用程序的入口点。
+//
+#include <iostream>
+#include <fstream>
+#include "Sales_item.h"
+#include "..\CppPrimer.h"
+
+std::ifstream file;
+void book_sales_data() {
+	file.open((std::string)PROJECT_SOURCE_DIR + "/SalesItem/book_sales");
+	std::cin.rdbuf(file.rdbuf());
+}
+
+
+int main()
+{
+	book_sales_data(); //cin重定向到文件
+	Sales_item total;
+	if (std::cin >> total) {
+		Sales_item trans;
+		while (std::cin >> trans)
+		{
+			if (total.isbn() == trans.isbn())
+				total += trans;
+			else
+			{
+				std::cout << total << std::endl;
+				total = trans;
+			}
+		}
+		std::cout << total << std::endl;
+	}
+	else
+	{
+		std::cerr << "No Data?!" << std::endl;
+		return -1;
+	}
+	return 0;
+}
